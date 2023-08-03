b75914deb5e714a8df7dcb83d75a1dd5affd48b1
Bethany Mattern <bethany.a.cable@gmail.com>
2023-08-03 16:13:45 -0400

    Componentize account forms and connect to schema

---
+++ src/routes/account/PasswordForm.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import LockIcon from "$lib/components/icons/LockIcon.svelte";
+	import type { PasswordSchema, passwordSchema } from "$lib/schemas";
+	import { error } from "@sveltejs/kit";
+	import { Button } from "flowbite-svelte";
+	import { superForm } from "sveltekit-superforms/client";
+	import type { Validation } from "sveltekit-superforms/index";
+	export let data: Validation<PasswordSchema>;
+
+	const { form, errors, enhance } = superForm(data);
+</script>
+
+<section class="px-6 pt-16">
+	<div class="flex items-center font-semibold">
+		<LockIcon />
+		<span class="ml-4">Password</span>
+	</div>
+	<form method="POST" action="?/updatePassword" use:enhance class="mt-8 flex flex-col space-y-4">
+		<label for="password" class="space-y-2">
+			<span>New Password</span>
+			<input type="password" name="password" bind:value={$form.password} />
+			{#if $errors.password}
+				<span class="block text-red-600 dark:text-red-500">{$errors.password}</span>
+			{/if}
+		</label>
+		<label for="confirmPassword" class="space-y-2">
+			<span>Confirm Password</span>
+			<input type="password" name="passowrdConfirm" bind:value={$form.passwordConfirm} />
+			{#if $errors.passwordConfirm}
+				<span class="block text-red-600 dark:text-red-500">{$errors.passwordConfirm}</span>
+			{/if}
+		</label>
+		<Button type="submit" class="mt-4 self-start">Change Password</Button>
+	</form>
+</section>
