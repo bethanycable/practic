bf175f812a8b8c19f5da538658c63349e93faa3a
Bethany Mattern <bethany.a.cable@gmail.com>
2023-08-15 22:01:21 -0400

    Add tier functionality in checkout

--- src/routes/api/stripe/checkout/+server.ts
+++ src/routes/api/stripe/checkout/+server.ts
@@ -1,12 +1,19 @@
+import { createCheckoutSession, getSubcriptionTier } from "$lib/server/subscriptions";
 import { error, redirect } from "@sveltejs/kit";
 
 import type { RequestHandler } from "@sveltejs/kit";
-import { createCheckoutSession } from "$lib/server/subscriptions";
+
+// import { handleLoginRedirect } from "$lib/helpers";
 
 export const GET: RequestHandler = async (event) => {
   const session = await event.locals.getSession();
   if(!session) {
     throw redirect(302, "/login");
+  }
+
+  const tier = await getSubcriptionTier(session.user.id);
+  if (tier !== "Free") {
+    throw redirect(302, "/account/billing");
   }
 
   const price_id = event.url.searchParams.get("id");
